ac25bedd4bc36fb89e4aa2053a13e22c310aa2fc
elizabethlin6 <elizabethlin@Elizabeths-MacBook-Pro.local>
2020-10-18 06:53:44 -0700

    Updates

---
+++ precache-manifest.bf88f12ef5055119963d80f2156b2b1e.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48572987f4eb87b994b6d06ae3b9e3b7",
+    "url": "/small-world/index.html"
+  },
+  {
+    "revision": "4a6952fc9153cd87a93d",
+    "url": "/small-world/static/css/main.63f8abaa.chunk.css"
+  },
+  {
+    "revision": "c9a7dd42c9650d064d9a",
+    "url": "/small-world/static/js/2.2b3216ff.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/small-world/static/js/2.2b3216ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a6952fc9153cd87a93d",
+    "url": "/small-world/static/js/main.0ff872fa.chunk.js"
+  },
+  {
+    "revision": "d5a291def633da4c756f",
+    "url": "/small-world/static/js/runtime-main.41244c9d.js"
+  },
+  {
+    "revision": "fdf0a0cd45caa9c472d68c1b6d4b51a7",
+    "url": "/small-world/static/media/down.fdf0a0cd.gif"
+  },
+  {
+    "revision": "ddff9210b40ade53c1dbc5839e2c0198",
+    "url": "/small-world/static/media/gs.ddff9210.png"
+  },
+  {
+    "revision": "028af17a608ac38b70686151ecd8e869",
+    "url": "/small-world/static/media/head.028af17a.png"
+  },
+  {
+    "revision": "178facf9ec5d041799229aad3e0f3b06",
+    "url": "/small-world/static/media/logo.178facf9.png"
+  },
+  {
+    "revision": "8f5ccb2b8583e591aae1db550e045af5",
+    "url": "/small-world/static/media/me.8f5ccb2b.png"
+  },
+  {
+    "revision": "c4558f6ec834a23484a518ba64e11c18",
+    "url": "/small-world/static/media/team.c4558f6e.png"
+  },
+  {
+    "revision": "13df7a2a72ff40d0ebad97e8e32ba3db",
+    "url": "/small-world/static/media/w1.13df7a2a.png"
+  },
+  {
+    "revision": "bab393e8f3f7f611b28cfbffa619392e",
+    "url": "/small-world/static/media/w2.bab393e8.png"
+  }
+]);
